aa20b748740e382ee5536af981be997a5141dd39
Jay Nielsen <jay.nielsen@sendgrid.com>
2020-07-22 14:53:22 -0600

    fixed workspace dependencies

--- packages/match-libraries/styling/rollup.config.js
+++ packages/match-libraries/styling/rollup.config.js
@@ -5,14 +5,14 @@ import commonjs from "@rollup/plugin-commonjs";
 import pkg from "./package.json";
 
 export default {
-  input: pkg["main:dev"],
+  input: pkg.main,
   output: [
     {
-      file: pkg.main,
+      file: pkg.publishConfig.main,
       format: "cjs",
     },
     {
-      file: pkg.module,
+      file: pkg.publishConfig.module,
       format: "esm",
     },
   ],

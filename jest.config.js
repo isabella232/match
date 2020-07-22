90a32b5cfdaef7b3634f764b6e18fbee60d45dc1
serenabux <sbuxton@twilio.com>
2020-07-22 14:45:50 -0600

    fixed naming for files and removed storyshots

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    "^.+\\.tsx?$": "ts-jest",
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-
+  preset: "ts-jest",
+  testEnvironment: "node",
 };

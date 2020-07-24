d476677af11e3822b851d9c9180ea6e1a7f647e1
serenabux <sbuxton@twilio.com>
2020-07-24 13:58:18 -0600

    updated jest test enviroment and test options

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
 };

be033bca1bc894ca3c65a604fa369ed212243afa
Jay Nielsen <jay.nielsen@sendgrid.com>
2020-07-31 14:57:15 -0600

    config cleanup

--- gatsby-node.js
+++
@@ -1,6 +0,0 @@
-exports.onCreateBabelConfig = ({ actions }) => {
-  actions.setBabelPlugin({
-    name: "@babel/plugin-proposal-class-properties",
-    name: "@babel/plugin-proposal-object-rest-spread",
-  });
-};

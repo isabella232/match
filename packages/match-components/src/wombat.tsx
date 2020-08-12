54df83ac4309c6e312a173faadee795b5f8f82cc
Jay Nielsen <jay.nielsen@sendgrid.com>
2020-08-12 15:51:06 -0600

    removed styling-library

--- packages/match-components/src/wombat.tsx
+++ packages/match-components/src/wombat.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as PropTypes from "prop-types";
-import { styled } from "@twilio-labs/match-styling-library";
+import styled from "styled-components";
 import { WombatDesignTokens } from "@twilio-labs/match-tokens";
 
 const ds = new WombatDesignTokens();

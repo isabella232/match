b85bbaddd9a40544309c48013f563e971e2ff73c
serenabux <sbuxton@twilio.com>
2020-08-14 16:33:53 -0600

    Removed lodash

--- packages/match-website/src/gatsby-theme-docz/index.js
+++ packages/match-website/src/gatsby-theme-docz/index.js
@@ -1,7 +1,6 @@
 import { ComponentsProvider, theme, useConfig } from "docz";
 import baseComponents from "gatsby-theme-docz/src/components";
 import defaultConfig from "gatsby-theme-docz/src/theme";
-import { merge } from "lodash";
 import React from "react";
 import { Styled, ThemeProvider } from "theme-ui";
 import { Link } from "../components/link";
@@ -23,34 +22,42 @@ const Theme = ({ children }) => {
   );
 };
 
-const themeConfig = merge(defaultConfig, {
+const themeConfig = {
+  ...defaultConfig,
   colors: {
+    ...defaultConfig.colors,
     primary: "#0263E0",
     text: "#050408",
     heading: "#0B0B10",
   },
   styles: {
+    ...defaultConfig.styles,
     h1: {
+      ...defaultConfig.styles.h1,
       fontSize: 36,
       color: "heading",
     },
     p: {
+      ...defaultConfig.styles.p,
       fontSize: 16,
       width: 778,
     },
     table: {
+      ...defaultConfig.styles.table,
       fontSize: 14,
       width: 778,
     },
     ul: {
+      ...defaultConfig.styles.ul,
       fontSize: 14,
     },
     pre: {
+      ...defaultConfig.styles.pre,
       fontSize: 14,
       width: 778,
     },
   },
-});
+};
 // export const matchTheme = theme(themeConfig)(Theme);
 // export const matchTheme = (themeConfig, Theme) => theme(themeConfig)(Theme);
 

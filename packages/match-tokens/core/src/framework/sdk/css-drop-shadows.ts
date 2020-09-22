d5f0c3809dec220d857313aed78a85d0c35eb2ae
serenabux <sbuxton@twilio.com>
2020-09-22 16:53:38 -0600

    Created DropShadows prefab

---
+++ packages/match-tokens/core/src/framework/sdk/css-drop-shadows.ts
@@ -0,0 +1,31 @@
+import { DropShadowsData } from "../prefabs/drop-shadows";
+import { colorToCss } from "@diez/web-sdk-common";
+
+/**
+ * Returns a string containing a valid CSS value for <box-shadow> and/or <text-shadow> from a [[Shadow]] prefab
+ * instance.
+ */
+export const dropShadowsToCss = (dropShadows: DropShadowsData) => {
+  const singleDropShadows = dropShadows.shadows.map((shadow) => {
+    return `${shadow.offset.x}px ${shadow.offset.y}px ${
+      shadow.radius
+    }px ${colorToCss(shadow.color)}`;
+  });
+  return singleDropShadows.join(" , ");
+};
+
+/**
+ * Returns a string containing a valid CSS <filter> value from a [[Shadow]] prefab instance.
+ */
+export const dropShadowsToFilterCss = (dropShadows: DropShadowsData) => {
+  // Since the `DropShadowData`'s radius value represents double the standard deviation of the desired Guassian blur,
+  // and `drop-shadow` expects the standard deviation, the value must be cut in half.
+  // See https://css-tricks.com/breaking-css-box-shadow-vs-drop-shadow/#comment-1612592
+  const singleDropShadows = dropShadows.shadows.map((shadow) => {
+    const radius = shadow.radius / 2;
+    return `drop-shadow(${shadow.offset.x}px ${
+      shadow.offset.y
+    }px ${radius}px ${colorToCss(shadow.color)})`;
+  });
+  return singleDropShadows.join(" , ");
+};

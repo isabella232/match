19730d9382bc60e65d4eaf1d92990ebf6280bb6a
Jay Nielsen <jay.nielsen@sendgrid.com>
2020-09-29 19:37:46 +0000

    updated button documentation

--- packages/match-components/src/button/types.ts
+++ packages/match-components/src/button/types.ts
@@ -19,8 +19,12 @@ export interface ButtonProps {
   variant?: ButtonVariant;
   type?: ButtonType;
   size?: ButtonSize;
+  /** A URL to route to. The button's 'type' attribute will be ignored. */
   href?: string;
+  /** Prevent actions from firing on the Button. */
   disabled?: boolean;
+  /** Sets the button width to 100% of the parent container. */
   fullWidth?: boolean;
+  /** Should the link trigger a download dialog? Must be used with the 'href' attribute. */
   download?: boolean;
 }

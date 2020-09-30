166bcd4517832b885cae058e726909ea73cbf2eb
Jay Nielsen <jay.nielsen@sendgrid.com>
2020-09-30 17:00:59 +0000

    added dark preview wrapper

--- packages/match-website/src/theme/ReactLiveScope/index.ts
+++ packages/match-website/src/theme/ReactLiveScope/index.ts
@@ -1,10 +1,20 @@
 import * as React from "react";
 import * as components from "@twilio-labs/match-components";
+import styled from "styled-components";
+
+const DarkPreview = styled.div`
+  margin: -1rem;
+  padding: 1rem;
+  background: ${({ theme }) => theme.background.blue.color};
+  border-bottom-right-radius: var(--ifm-global-radius);
+  border-bottom-left-radius: var(--ifm-global-radius);
+`;
 
 const ReactLiveScope = {
   React,
   ...React,
   ...components,
+  DarkPreview,
 };
 
 export default ReactLiveScope;

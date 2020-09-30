d475f8e89a87cf6bb079f15d0c25db93eda2fd7c
Jay Nielsen <jay.nielsen@sendgrid.com>
2020-09-30 17:42:24 +0000

    allow multiple bg colors for dark preview

--- packages/match-website/src/theme/ReactLiveScope/index.ts
+++ packages/match-website/src/theme/ReactLiveScope/index.ts
@@ -2,13 +2,21 @@ import * as React from "react";
 import * as components from "@twilio-labs/match-components";
 import styled from "styled-components";
 
-const DarkPreview = styled.div`
+interface DarkPreviewProps {
+  bg: "blue" | "darkest";
+}
+
+const DarkPreview = styled.div<DarkPreviewProps>`
   margin: -1rem;
   padding: 1rem;
-  background: ${({ theme }) => theme.background.blue.color};
+  background: ${({ bg, theme }) => theme.background[bg].color};
   border-bottom-right-radius: var(--ifm-global-radius);
   border-bottom-left-radius: var(--ifm-global-radius);
 `;
+
+DarkPreview.defaultProps = {
+  bg: "darkest",
+};
 
 const ReactLiveScope = {
   React,

ac1fcfd2343e2979dd02c9baaaf160be04183063
Jay Nielsen <jay.nielsen@sendgrid.com>
2020-10-09 20:03:41 +0000

    hide variant option

--- packages/match-components/stories/button.stories.tsx
+++ packages/match-components/stories/button.stories.tsx
@@ -14,6 +14,7 @@ export default {
   },
   argTypes: {
     children: { table: { disable: true } },
+    variant: { table: { disable: true } },
     size: {
       control: { type: "select", options: Object.values(ButtonSize) },
     },

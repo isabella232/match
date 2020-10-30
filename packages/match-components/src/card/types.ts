81f5f35810252350054e1ff6da307972f1122624
serenabux <sbuxton@twilio.com>
2020-10-30 14:16:57 -0600

    Added padding and margins to card story

--- packages/match-components/src/card/types.ts
+++ packages/match-components/src/card/types.ts
@@ -8,14 +8,24 @@ export enum CardVariant {
 
 export interface CardProps extends React.HTMLAttributes<HTMLElement> {
   variant?: CardVariant;
+  /** scale100 | scale180 | scale260 | scale340 | scale420 | scale540 */
   padding?: SpaceOptions;
+  /** scale100 | scale180 | scale260 | scale340 | scale420 | scale540 */
   paddingLeft?: SpaceOptions;
+  /** scale100 | scale180 | scale260 | scale340 | scale420 | scale540 */
   paddingRight?: SpaceOptions;
+  /** scale100 | scale180 | scale260 | scale340 | scale420 | scale540 */
   paddingBottom?: SpaceOptions;
+  /** scale100 | scale180 | scale260 | scale340 | scale420 | scale540 */
   paddingTop?: SpaceOptions;
+  /** scale100 | scale180 | scale260 | scale340 | scale420 | scale540 */
   margin?: SpaceOptions;
+  /** scale100 | scale180 | scale260 | scale340 | scale420 | scale540 */
   marginLeft?: SpaceOptions;
+  /** scale100 | scale180 | scale260 | scale340 | scale420 | scale540 */
   marginRight?: SpaceOptions;
+  /** scale100 | scale180 | scale260 | scale340 | scale420 | scale540 */
   marginBottom?: SpaceOptions;
+  /** scale100 | scale180 | scale260 | scale340 | scale420 | scale540 */
   marginTop?: SpaceOptions;
 }
--- packages/match-components/stories/card.stories.tsx
+++ packages/match-components/stories/card.stories.tsx
@@ -1,16 +1,27 @@
 import * as React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import { Card, CardProps, CardVariant } from "../src";
+import { TwilioDesignTokens } from "@twilio-labs/match-tokens";
+
+const DS = new TwilioDesignTokens();
 
 export default {
   title: "Components/Card",
   component: Card,
   args: {
     children: "Ahoy",
+    padding: "scale100",
+    margin: "scale100",
   },
   argTypes: {
     children: { table: { disable: true } },
     variant: { table: { disable: true } },
+    padding: {
+      control: { type: "select", options: Object.keys(DS.space) },
+    },
+    margin: {
+      control: { type: "select", options: Object.keys(DS.space) },
+    },
   },
 } as Meta;
 

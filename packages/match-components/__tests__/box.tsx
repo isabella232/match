b6a40cf2439993db89640bcd754d323bf3a5dbbc
serenabux <sbuxton@twilio.com>
2020-10-30 14:33:44 -0600

    Wrote tests

--- packages/match-components/__tests__/box.tsx
+++ packages/match-components/__tests__/box.tsx
@@ -2,6 +2,7 @@ import "jest-styled-components";
 import * as React from "react";
 import { render } from "@testing-library/react";
 import { withTheme } from "@twilio-labs/match-themes";
+import { axe } from "jest-axe";
 import { Box } from "../src";
 
 const BoxWithTheme = withTheme()(Box);
@@ -12,5 +13,15 @@ describe("Spaces", () => {
       <BoxWithTheme padding="scale20">space20</BoxWithTheme>
     );
     expect(padding).toMatchSnapshot();
+  });
+
+  test("accessibility violations", async () => {
+    const { container } = render(
+      <BoxWithTheme padding="scale100">
+        Claustrophobic people are more productive thinking out of the box.
+      </BoxWithTheme>
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });

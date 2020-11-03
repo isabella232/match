9b83c691cf507637a47317bfa34b6d2146a633f8
Jay Nielsen <jay.nielsen@sendgrid.com>
2020-11-03 22:13:10 +0000

    updated match themes package

--- packages/match-themes/__tests__/with-theme.test.tsx
+++ packages/match-themes/__tests__/with-theme.test.tsx
@@ -1,33 +1,31 @@
 import * as React from "react";
 import { render } from "@testing-library/react";
-import {
-  TwilioDesignTokens,
-  SignalDesignTokens,
-} from "@twilio-labs/match-tokens";
+import * as TwilioDesignTokens from "@twilio-labs/match-tokens/twilio";
+import * as SendGridDesignTokens from "@twilio-labs/match-tokens/sendgrid";
 import { withTheme, ThemeConsumer, ThemeVariants } from "../src";
 
 const ThemeConsumerExample: React.FC = () => {
   return (
     <ThemeConsumer>
-      {({ theme }) => <p data-testid="color">{theme.swatch.brand.color}</p>}
+      {({ theme }) => <p data-testid="color">{theme.colorBrand}</p>}
     </ThemeConsumer>
   );
 };
 
 describe("withTheme provider", () => {
   test("should render the Twilio brand color", (): void => {
-    const tokens = new TwilioDesignTokens();
+    const { colorBrand } = TwilioDesignTokens;
     const WithTwilioTheme = withTheme()(ThemeConsumerExample);
     const { getByTestId } = render(<WithTwilioTheme />);
-    expect(getByTestId("color")).toHaveTextContent(tokens.swatch.brand.color);
+    expect(getByTestId("color")).toHaveTextContent(colorBrand);
   });
 
-  test("should render the Signal brand color", (): void => {
-    const tokens = new SignalDesignTokens();
-    const WithSignalTheme = withTheme(ThemeVariants.SIGNAL)(
+  test("should render the SendGrid brand color", (): void => {
+    const { colorBrand } = SendGridDesignTokens;
+    const WithSignalTheme = withTheme(ThemeVariants.SENDGRID)(
       ThemeConsumerExample
     );
     const { getByTestId } = render(<WithSignalTheme />);
-    expect(getByTestId("color")).toHaveTextContent(tokens.swatch.brand.color);
+    expect(getByTestId("color")).toHaveTextContent(colorBrand);
   });
 });
--- packages/match-themes/src/index.ts
+++ packages/match-themes/src/index.ts
@@ -1,19 +1,12 @@
 import { ThemeProvider } from "styled-components";
-import {
-  TwilioDesignTokens,
-  SendGridDesignTokens,
-  SignalDesignTokens,
-  AhoyDesignTokens,
-} from "@twilio-labs/match-tokens";
+import * as TwilioDesignTokens from "@twilio-labs/match-tokens/twilio";
+import * as SendGridDesignTokens from "@twilio-labs/match-tokens/sendgrid";
+import * as AhoyDesignTokens from "@twilio-labs/match-tokens/ahoy";
 import { withTokens } from "./with-tokens";
 
-const TwilioTheme = withTokens(new TwilioDesignTokens())(ThemeProvider);
-
-const SendGridTheme = withTokens(new SendGridDesignTokens())(ThemeProvider);
-
-const SignalTheme = withTokens(new SignalDesignTokens())(ThemeProvider);
-
-const AhoyTheme = withTokens(new AhoyDesignTokens())(ThemeProvider);
+const TwilioTheme = withTokens(TwilioDesignTokens)(ThemeProvider);
+const SendGridTheme = withTokens(SendGridDesignTokens)(ThemeProvider);
+const AhoyTheme = withTokens(AhoyDesignTokens)(ThemeProvider);
 
 export { ThemeVariants } from "./constants";
 export { MatchThemeProvider } from "./theme-provider";
@@ -21,4 +14,4 @@ export { StyledBase } from "./styles";
 export { withTheme } from "./with-theme";
 export { ThemeConsumer } from "./theme-consumer";
 export { useTheme } from "./use-theme";
-export { TwilioTheme, SendGridTheme, SignalTheme, AhoyTheme };
+export { TwilioTheme, SendGridTheme, AhoyTheme };

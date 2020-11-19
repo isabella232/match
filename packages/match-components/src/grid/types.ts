94afa7ce1023bae6c1b8494ff423c1b423950084
serenabux <sbuxton@twilio.com>
2020-11-19 10:36:40 -0700

    docs: added grid documentation

--- packages/match-components/src/grid/types.ts
+++ packages/match-components/src/grid/types.ts
@@ -11,19 +11,70 @@ export interface GridProps
   extends React.HTMLAttributes<HTMLDivElement>,
     PaddingProps,
     MarginProps {
+  /**
+   * Sets the gap between rows and columns. Suggested values: 'space420', 'space260', 'space180', 'space100'
+   * @type Space
+   */
   gridGap?: SpaceProp;
+  /**
+   * Sets the gap between rows. Suggested values: 'space420', 'space260', 'space180', 'space100'
+   * @type Space
+   */
   gridRowGap?: SpaceProp;
+  /**
+   * Sets the gap between columns. Suggested values: 'space420', 'space260', 'space180', 'space100'
+   * @type Space
+   */
   gridColumnGap?: SpaceProp;
-  gridColumn?: ResponsiveValue<CSS.Property.GridColumn>;
+  /**
+   * Specifies how auto-placed items get placed in the grid
+   * @type String
+   */
   gridAutoFlow?: ResponsiveValue<CSS.Property.GridAutoFlow>;
+  /**
+   * Specifies the size of an implicitly-created grid row track or pattern of tracks
+   * @type String
+   */
   gridAutoRows?: ResponsiveValue<CSS.Property.GridAutoRows>;
+  /**
+   * Specifies the size of an implicitly-created grid column track or pattern of tracks
+   * @type String
+   */
   gridAutoColumns?: ResponsiveValue<CSS.Property.GridAutoColumns>;
+  /**
+   * Defines the line names and track sizing functions of the grid rows
+   * @type String
+   */
   gridTemplateRows?: ResponsiveValue<CSS.Property.GridTemplateRows>;
+  /**
+   * Defines the line names and track sizing functions of the grid columns
+   * @type String
+   */
   gridTemplateColumns?: ResponsiveValue<CSS.Property.GridTemplateColumns>;
+  /**
+   * Specifies named grid areas, establishing the cells in the grid and assigning them names
+   * @type String
+   */
   gridTemplateAreas?: ResponsiveValue<CSS.Property.GridTemplateAreas>;
+  /**
+   * Sets the align-self value for all children
+   * @type String
+   */
   alignItems?: ResponsiveValue<CSS.Property.AlignItems>;
+  /**
+   * Sets the distribution of space between and around the grid’s children along the cross/block axis
+   * @type String
+   */
   alignContent?: ResponsiveValue<CSS.Property.AlignContent>;
+  /**
+   * Sets the justify-self value for all children
+   * @type String
+   */
   justifyItems?: ResponsiveValue<CSS.Property.JustifyItems>;
+  /**
+   * Sets distribution of space between and around the gird’s children along the main-axis/inline axis
+   * @type String
+   */
   justifyContent?: ResponsiveValue<CSS.Property.JustifyContent>;
 }
 
@@ -31,11 +82,39 @@ export interface CellProps
   extends React.HTMLAttributes<HTMLDivElement>,
     PaddingProps,
     MarginProps {
+  /**
+   * Aligns the cell in the grid along the appropriate axis
+   * @type String
+   */
   alignSelf?: ResponsiveValue<CSS.Property.AlignSelf>;
+  /**
+   * Sets the justification of the cell in the grid along the appropriste axis
+   * @type String
+   */
   justifySelf?: ResponsiveValue<CSS.Property.JustifySelf>;
+  /**
+   * Set the order of the cells. Cells in the same grid are sorted in ascending order.
+   * @type Int
+   */
   order?: ResponsiveValue<CSS.Property.Order>;
+  /**
+   * Specifies the cell’s size and location within the column of a grid
+   * @type String
+   */
   gridColumn?: ResponsiveValue<CSS.Property.GridColumn>;
+  /**
+   * Specifies the cell’s size and location within the row of a grid
+   * @type String
+   */
   gridRow?: ResponsiveValue<CSS.Property.GridRow>;
+  /**
+   * Specifies cell's size and location within a grid
+   * @type String
+   */
   gridArea?: ResponsiveValue<CSS.Property.GridArea>;
+  /**
+   * Sets the cell's background color
+   * @type BackgroundColors
+   */
   backgroundColor?: BackgroundColorProp;
 }

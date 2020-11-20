0a2a9adc506d7156a48ef9d637cfbede33e846ba
serenabux <sbuxton@twilio.com>
2020-11-20 10:27:59 -0700

    docs: added expansion for props

--- packages/match-website/src/components/props.tsx
+++ packages/match-website/src/components/props.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
+import { useState } from "react";
 import { ComponentDoc } from "react-docgen-typescript";
 import { usePluginData } from "@docusaurus/useGlobalData";
+import styles from "./styles.module.css";
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from "@twilio-labs/match-icons-twilio";
 
 interface PropsProps {
   of: string;
@@ -69,7 +75,20 @@ const Props: React.FC<PropsProps> = ({ of }) => {
     );
   }, [docgens, of]);
 
+  /**
+   * Toggle the expansion of the props table when the button is clicked
+   * The table will have an expansion option if there are more than 8 props
+   * If it has the expansion button, it will initially show 5 props
+   */
+  const [isExpanded, setExpanded] = useState(false);
+
+  const handlePreview = (e) => {
+    e.preventDefault();
+    setExpanded(!isExpanded); // Here we change state
+  };
+
   if (props.length === 0) return <p>No component props found for {of} 😔</p>;
+  const expand = props.length > 8 ? true : false;
 
   return (
     <table>
@@ -82,14 +101,52 @@ const Props: React.FC<PropsProps> = ({ of }) => {
         </tr>
       </thead>
       <tbody>
-        {props.map((prop) => (
-          <tr key={prop.name}>
-            <td>{prop.name}</td>
-            <td>{prop.type}</td>
-            <td>{prop.default}</td>
-            <td>{prop.description}</td>
+        {props.map((prop, index) => {
+          if (!expand || index < 5 || isExpanded) {
+            return (
+              <tr key={prop.name}>
+                <td>{prop.name}</td>
+                <td>{prop.type}</td>
+                <td>{prop.default}</td>
+                <td>{prop.description}</td>
+              </tr>
+            );
+          }
+        })}
+        {expand && !isExpanded && (
+          <tr key="expand">
+            <td colSpan={4}>
+              <button
+                className={styles.propsExpand}
+                onClick={(e) => handlePreview(e)}
+              >
+                See all {props.length} props
+                <ChevronDownIcon
+                  color="blue60"
+                  size="small"
+                  className={styles.iconExpand}
+                />
+              </button>
+            </td>
           </tr>
-        ))}
+        )}
+        {expand && isExpanded && (
+          <tr key="expand">
+            <td colSpan={4}>
+              <button
+                className={styles.propsExpand}
+                onClick={(e) => handlePreview(e)}
+              >
+                See less props
+                <ChevronUpIcon
+                  color="blue60"
+                  size="small"
+                  className={styles.iconExpand}
+                />
+              </button>
+            </td>
+          </tr>
+        )}
       </tbody>
     </table>
   );
--- packages/match-website/src/theme/ReactLiveScope/index.ts
+++ packages/match-website/src/theme/ReactLiveScope/index.ts
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as components from "@twilio-labs/match-components";
 import { ChatIcon, TerminalIcon } from "@twilio-labs/match-icons-twilio";
-import { FlexIcon } from "@twilio-labs/match-icons-product";
 
 const ReactLiveScope = {
   React,
@@ -9,7 +8,6 @@ const ReactLiveScope = {
   ...components,
   ChatIcon,
   TerminalIcon,
-  FlexIcon,
 };
 
 export default ReactLiveScope;

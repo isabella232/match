6bb6dbb1a43038f3fae4897e791f046a497bb0cc
Gabriel Peracio <gabriel@sigsonic.com>
2020-11-30 21:39:32 +0000

    docs: added documentation for separator

--- packages/match-components/src/separator/index.tsx
+++ packages/match-components/src/separator/index.tsx
@@ -3,6 +3,7 @@ import * as PropTypes from "prop-types";
 import { marginPropTypes } from "@twilio-labs/match-props";
 import { StyledSeparator } from "./styles";
 import { SeparatorProps, SeparatorVariant } from "./types";
+import { space } from "@twilio-labs/match-tokens";
 
 const Separator: React.FC<SeparatorProps> = (props) => (
   <StyledSeparator {...props} />
@@ -14,6 +15,11 @@ Separator.propTypes = {
 };
 
 Separator.displayName = "Separator";
+
+Separator.defaultProps = {
+  variant: SeparatorVariant.PRIMARY,
+  marginY: space.scale20,
+};
 
 export { Separator, SeparatorVariant };
 export type { SeparatorProps };

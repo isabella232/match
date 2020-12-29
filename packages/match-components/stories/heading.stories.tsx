7c51a95af666050c147ab04001549e8c86ebf851
Jay Nielsen <jay.nielsen@sendgrid.com>
2020-12-29 20:36:23 +0000

    test(heading): added inverse story

--- packages/match-components/stories/heading.stories.tsx
+++ packages/match-components/stories/heading.stories.tsx
@@ -7,16 +7,30 @@ export default {
   component: Heading,
   args: {
     children: "Ahoy World!",
+    color: "inherit",
     variant: "h1",
   },
   argTypes: {
     children: { table: { disable: true } },
+    color: { table: { disable: true } },
     variant: {
       control: { type: "select", options: Object.values(HeadingVariant) },
     },
   },
 } as Meta;
 
-const Template: Story<HeadingProps> = (args) => <Heading {...args} />;
+const Template: Story<HeadingProps> = ({ color, ...args }) => (
+  <div style={{ color }}>
+    <Heading {...args} />
+  </div>
+);
 
 export const Primary = Template.bind({});
+
+export const Inverse: Story<HeadingProps> = Template.bind({});
+Inverse.args = {
+  color: "#fff",
+};
+Inverse.parameters = {
+  backgrounds: { default: "Darkest" },
+};

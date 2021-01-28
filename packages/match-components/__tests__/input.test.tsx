eae2ae0eb82b7c5e690ed230b47bd85e20898665
Jay Nielsen <jay.nielsen@sendgrid.com>
2021-01-28 18:54:47 +0000

    test(input): added validation api test

--- packages/match-components/__tests__/input.test.tsx
+++ packages/match-components/__tests__/input.test.tsx
@@ -1,12 +1,43 @@
 import * as React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import { axe } from "jest-axe";
 import { withTheme } from "@twilio-labs/match-themes";
 import { Input } from "../src";
 
 const InputWithTheme = withTheme()(Input);
 
+const InputWithValidation: React.FC = () => {
+  const [error, setError] = React.useState("");
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { validity } = e.currentTarget;
+    if (validity.typeMismatch) {
+      setError("Email format is invalid");
+    } else {
+      setError("");
+    }
+  };
+  return (
+    <InputWithTheme
+      data-testid="email"
+      name="email"
+      type="email"
+      label="Email"
+      error={error}
+      onChange={onInputChange}
+    />
+  );
+};
+
 describe("Input", () => {
+  test("validation api", () => {
+    render(<InputWithValidation />);
+    userEvent.type(screen.getByTestId('email'), 'foo');
+    expect(screen.getByRole('alert')).toHaveTextContent(/email format is invalid/i);
+    userEvent.type(screen.getByTestId('email'), '@bar.com');
+    expect(screen.queryByRole('alert')).toBeNull();
+  });
+
   test("helper message", () => {
     const { getByText } = render(
       <InputWithTheme name="example" label="Example" helper="helper" />
